830257fff66eafee209b9aa0143d69d29cb0fc07
ehsAnhAq86 <ehsunhagh86@gmail.com>
2024-07-30 13:01:38 +0330

    feat:datasource config

--- back-end/src/Utility/data-source.ts
+++ back-end/src/Utility/data-source.ts
@@ -1,19 +1,20 @@
-import {DataSource} from "typeorm"
+import { DataSource } from "typeorm"
+import dotenv from "dotenv-flow"
+
+dotenv.config()
 export const AppDataSource = new DataSource({
 
     type: "mysql",
     host: "localhost",
     port: 3306,
-    
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+})
 
 
-
-
-
-
-
-
-
-
-
-}) 
