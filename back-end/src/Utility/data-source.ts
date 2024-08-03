3f81e5e4c84501450b4a9f56faf539246962cbe4
ehsAnhAq86 <ehsunhagh86@gmail.com>
2024-08-03 11:30:05 +0330

    ehs/fix-some-bugs

--- back-end/src/Utility/data-source.ts
+++ back-end/src/Utility/data-source.ts
@@ -3,13 +3,8 @@ import dotenv from "dotenv-flow";
 import { UserEntity } from "../DB-Entities/user.entity";
 import { FormEntity } from "../DB-Entities/form.entity";
 import { FieldEntity } from "../DB-Entities/Field.entity";
-<<<<<<< HEAD
 import { AnswersEntity } from "../DB-Entities/asnwers.entity";
 import { AnswerSheetEntity } from "../DB-Entities/answer-sheets.entity";
-=======
-import { AnswerEntity } from "../DB-Entities/asnwers.entity";
-import { AnswerSheetEntity } from "../DB-Entities/answer-sheet.entity";
->>>>>>> feature/no-ref/add-types-and-getbyid-function
 
 dotenv.config();
 export const AppDataSource = new DataSource({
@@ -20,20 +15,12 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     synchronize: true,
-<<<<<<< HEAD
     logging: false,
-=======
-    logging: true,
->>>>>>> feature/no-ref/add-types-and-getbyid-function
     entities: [
         UserEntity,
         FormEntity,
         FieldEntity,
-<<<<<<< HEAD
         AnswersEntity,
-=======
-        AnswerEntity,
->>>>>>> feature/no-ref/add-types-and-getbyid-function
         AnswerSheetEntity,
     ],
     subscribers: [],
